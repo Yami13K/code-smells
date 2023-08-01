c9e5ed3af83abe71491c6ea4a108db6f9d050624
Yamen kayyal <alkayyalyamen@gmail.com>
2023-08-01 16:16:33 +0300

    fix csv path

--- config/paths.py
+++ config/paths.py
@@ -1,5 +1,5 @@
 IMAGE_URL = 'https://training.javatpoint.com/uploads/blogs/d08f92e69686eb85e439516c47af2203.png'
-DS_PATH = './dataset.csv'
+DS_PATH = 'csv/dataset.csv'
 
 #APP
 APP_TITLE = 'ReliaMain'
