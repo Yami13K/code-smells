e089da1b202365828b3b16040c47a7d8d814291d
Yamen kayyal <alkayyalyamen@gmail.com>
2023-08-03 13:43:32 +0300

    fixed loading issue

--- codes/clustering.py
+++ codes/clustering.py
@@ -1,4 +1,3 @@
-
 from sklearn.cluster import KMeans
 
 from codes.utils.clustering import *
@@ -12,7 +11,7 @@ def clusterize(scores_df):
         title.title("Clustering Metrics Visualisation")
 
     if scores_df is not None:
-        spinner = st.spinner("Clustering...")
+        spinner = st.spinner("Visualising...")
 
         with open("static/clustering.css", "r") as css_file:
             st.markdown(f"<style>{css_file.read()}</style>", unsafe_allow_html=True)
@@ -28,7 +27,7 @@ def clusterize(scores_df):
         with text_col:
             clusters_input = st.empty()
             cluster_num = clusters_input.number_input(
-                ' clustering num',min_value=2, max_value=10, value=3
+                " clustering num", min_value=2, max_value=10, value=3
             )
         _, button_col = st.columns([10, 20])
 
@@ -38,43 +37,21 @@ def clusterize(scores_df):
         button = button_placeholder.button("Show Metrics!")
         if button:
             reset()
-            _, text_col, _ = st.columns([4, 2.5, 5])
-            with text_col:
-                with spinner:
-                    result, centroids = kmeans(scores_df, cluster_num)
-            st.balloons()
 
-            centroids_plot(result, centroids)
-            custom_distribution_plot(result)
-            elbow_plot(scores_df)
-            data = np.array(scores_df['Score']).reshape(-1,1)
-            label = np.array(scores_df['Cluster'])
-            plot_silhouette(data, label)
+            with spinner:
+                result, centroids = kmeans(scores_df, cluster_num)
+                custom_distribution_plot(result)
+                centroids_plot(result, centroids)
+                elbow_plot(scores_df)
+                data = np.array(scores_df["Score"]).reshape(-1, 1)
+                label = np.array(scores_df["Cluster"])
+                plot_silhouette(data, label)
+            # st.balloons()
 
             _, button_col = st.columns([13, 20])
-            # reset_button_style = """
-            #     background-color: #f44336; /* Red color */
-            #     color: white;
-            #     padding: 0.5rem 1rem;
-            #     border: none;
-            #     border-radius: 0.25rem;
-            #     cursor: pointer;
-            #     transition: background-color 0.3s ease;
-            #     font-size: 1rem;
-            #     font-weight: bold;
-            #     text-align: center;
-            #     text-decoration: none;
-            #     display: inline-block;
-            #     margin: 0.2rem;
-            # """
-
-            # Apply the style to the reset button using the 'style' parameter
             with button_col:
                 if st.button("Reset", key="reset_button"):
                     clusterize(scores_df)
-
-
-
 
     else:
         st.warning(WARNING)
